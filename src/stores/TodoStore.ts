74b56a2501caa9b6d53526ceda3b6c53849d55eb
abchen <abchen9527@outlook.com>
2021-09-03 22:19:45 +0800

    去除TodoStore装饰器语法

--- src/stores/TodoStore.ts
+++ src/stores/TodoStore.ts
@@ -15,17 +15,14 @@ export class TodoStore {
         this.todos = [];
     }
 
-    @computed
     get completedTodos(){
         return this.todos.filter(todo=>todo.finished)
     }
 
-    @action
     addTodo(title: string) {
         this.todos.push(new Todo(this, title, false));
     }
 
-    @action
     toggleAll(checked: boolean) {
         this.todos.forEach((todo) => (todo.finished = checked));
     }
@@ -34,7 +31,6 @@ export class TodoStore {
         return this.todos.map((todo) => todo.toJs());
     }
 
-    @action
     clearFinished() {
         this.todos = this.todos.filter((todo) => !todo.finished);
     }
